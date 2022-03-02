61d44a398595f887975d2b47ae014539c2c95b6e
Justin <ja7wong@uwaterloo.ca>
2022-03-02 17:48:01 -0500

    updated

--- backend/Server/db_initialization.sql
+++ backend/Server/db_initialization.sql
@@ -16,42 +16,46 @@ CREATE TABLE login_credentials (
 );
 
 CREATE TABLE geographic_data (
-    location_code INT PRIMARY KEY,
-    location VARCHAR (255) NOT NULL,
+    location_code SERIAL PRIMARY KEY,
+    location VARCHAR (255) UNIQUE,
     city VARCHAR (255) NOT NULL,
     country VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE attraction_data (
-    attraction_id INT PRIMARY KEY,
-    attraction_name VARCHAR (255) NOT NULL,
-    rating REAL DEFAULT 1,
-    location_code INT REFERENCES geographic_data,
-    country VARCHAR (255) NOT NULL
-);
-
-CREATE TABLE restaurant_data (
-    restaurant_id INT PRIMARY KEY,
-    restaurant_name VARCHAR (255) NOT NULL,
+    attraction_id SERIAL PRIMARY KEY,
+    attraction_name VARCHAR (255) UNIQUE,
     rating REAL DEFAULT 1,
     location_code INT REFERENCES geographic_data,
     country VARCHAR (255) NOT NULL,
+    labels TEXT DEFAULT NULL
+);
+
+CREATE TABLE restaurant_data (
+    restaurant_id SERIAL PRIMARY KEY,
+    restaurant_name VARCHAR (255) UNIQUE,
+    rating REAL DEFAULT 1,
+    location_code INT REFERENCES geographic_data,
+    country VARCHAR (255) NOT NULL,
+    labels TEXT DEFAULT NULL,
     source_site_id INT DEFAULT NULL
 );
 
 CREATE TABLE restaurant_review_data (
-    review_id INT PRIMARY KEY,
+    review_id SERIAL PRIMARY KEY,
     review_text TEXT DEFAULT NULL,
     rating REAL DEFAULT 1,
     review_date TIMESTAMP DEFAULT NULL,
-    restaurant_id INT REFERENCES restaurant_data
+    restaurant_id INT REFERENCES restaurant_data,
+    labels TEXT DEFAULT NULL
 );
 
 CREATE TABLE attraction_review_data (
-    review_id INT PRIMARY KEY,
+    review_id SERIAL PRIMARY KEY,
     review_text TEXT DEFAULT NULL,
     rating REAL DEFAULT 1,
     review_date TIMESTAMP DEFAULT NULL,
+    labels TEXT DEFAULT NULL,
     attraction_id INT REFERENCES restaurant_data
 );
 
